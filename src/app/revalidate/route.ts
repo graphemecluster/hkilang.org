cb66f6360aebf34fb2617c857ad0b0c0ab1040cc
graphemecluster <graphemecluster@gmail.com>
2025-06-04 09:08:17 +0800

    Update page revalidation webhook to match new data structure

--- src/app/revalidate/route.ts
+++ src/app/revalidate/route.ts
@@ -14,27 +14,31 @@ export async function POST(request: NextRequest) {
 	try {
 		const body = await request.json();
 		const { event, entry, model } = body;
+		const isEntryCreation = event === "entry.create";
 		switch (event.split(".")[0]) {
 			case "entry":
 				switch (model) {
 					// Collection types
 					case "article": {
 						const category = entry?.category?.slug;
-						if (category !== "news") return new NextResponse("Not a news article, revalidation not needed");
 						revalidatePath("/");
 						revalidatePath(`/${category}`);
 						revalidatePath(`/${category}/${entry?.slug}`);
 						break;
 					}
 					case "category": {
-						const category = entry?.slug;
-						if (category !== "news") return new NextResponse("Not the news category, revalidation not needed");
-						revalidatePath(`/${category}`);
-						// Revalidate all news pages
-						revalidatePath(`/${category}/[slug]`, "page");
+						if (isEntryCreation) return new NextResponse("Revalidation not needed for category creation"); // New categories won't be visible at all because navigation items are currently hard coded.
+						revalidatePath(`/${entry?.slug}`);
+						revalidatePath("/[category]/[slug]", "page"); // Revalidate all article detail pages (TODO only revalidate articles with this category)
+						break;
+					}
+					case "genre": {
+						if (isEntryCreation) return new NextResponse("Revalidation not needed for genre creation"); // because genres without themed items are hidden. The language pages will be revalidated upon creation of themed items with this genre.
+						revalidatePath("/languages/[slug]", "page"); // Revalidate all language detail pages
 						break;
 					}
 					case "language": {
+						if (isEntryCreation) return new NextResponse("Revalidation not needed for language creation"); // because languages without any references are not visible at all.
 						revalidatePath("/");
 						revalidatePath("/languages");
 						revalidatePath(`/languages/${entry?.slug}`);
@@ -46,22 +50,35 @@ export async function POST(request: NextRequest) {
 						revalidatePath(`/languages/${entry?.lang?.slug}`);
 						break;
 					}
+					case "lexical-item": {
+						// We don't know if a surveyed/showcase lexical domain is being added/removed or not, so we'll have to revalidate both for safety
+						revalidatePath("/dictionary");
+						revalidatePath("/languages/[slug]", "page"); // Revalidate all language detail pages
+						break;
+					}
 					case "listed-character":
-					case "surveyed-lexical-domain":
-					case "surveyed-lexical-item": {
+					case "listed-syllable":
+					case "surveyed-lexical-domain": {
 						revalidatePath("/dictionary");
 						break;
 					}
+					case "showcase-lexical-domain": {
+						if (isEntryCreation) return new NextResponse("Revalidation not needed for showcase lexical domain creation"); // because domains without lexical items are hidden. The language pages will be revalidated upon creation of lexical items with this domain.
+						revalidatePath("/languages/[slug]", "page"); // Revalidate all language detail pages
+						break;
+					}
 					case "tag": {
-						revalidatePath("/news");
-						// Revalidate all news pages
-						revalidatePath(`/news/[slug]`, "page");
-						// TODO only revalidate news with this tag
+						if (isEntryCreation) return new NextResponse("Revalidation not needed for tag creation"); // because tags without articles are hidden. The article pages will be revalidated upon creation of articles with this tag.
+						revalidatePath("/[category]", "page"); // Revalidate all article pages
+						revalidatePath("/[category]/[slug]", "page"); // Revalidate all article detail pages (TODO only revalidate articles with this tag)
+						break;
+					}
+					case "themed-item": {
+						revalidatePath("/languages/[slug]", "page"); // Revalidate all language detail pages
 						break;
 					}
 					case "word-of-the-day": {
-						// Do not revalidate if it's a future entry
-						if (new Date(`${entry?.date}T00:00:00+08:00`) > new Date()) return new NextResponse("Future entry, revalidation not needed");
+						if (new Date(`${entry?.date}T00:00:00+08:00`) > new Date()) return new NextResponse("Revalidation not needed for future word of the day entry");
 						revalidatePath("/");
 						revalidatePath("/dictionary");
 						break;
@@ -72,15 +89,14 @@ export async function POST(request: NextRequest) {
 						break;
 					}
 					case "contact-section": {
-						// Revalidate ALL pages
-						revalidatePath("/", "layout");
+						revalidatePath("/", "layout"); // Revalidate ALL pages
 						break;
 					}
 					case "dictionary-page": {
 						revalidatePath("/dictionary");
 						break;
 					}
-					case "faq": {
+					case "faq-page": {
 						revalidatePath("/faq");
 						break;
 					}
@@ -90,10 +106,6 @@ export async function POST(request: NextRequest) {
 					}
 					case "languages-page": {
 						revalidatePath("/languages");
-						break;
-					}
-					case "news-page": {
-						revalidatePath("/news");
 						break;
 					}
 				}
