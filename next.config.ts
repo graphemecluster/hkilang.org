da042ce940d6d19811d1bd864ba89baafe915602
graphemecluster <graphemecluster@gmail.com>
2025-06-12 18:37:01 +0800

    Add `Object.groupBy` polyfill
    This is a temporary solution. Usage-based auto polyfill should be implemented in the future.

--- next.config.ts
+++ next.config.ts
@@ -18,4 +18,15 @@ export default {
 	async rewrites() {
 		return ["/api/:path*", "/uploads/:path*"].map(source => ({ source, destination: `${new URL(source, STRAPI_URL)}` }));
 	},
+	webpack(config, { isServer }) {
+		const originalEntry = config.entry;
+		config.entry = async () => {
+			const entries = await originalEntry();
+			if (!isServer && entries["main.js"] && !entries["main.js"].includes("./src/lib/polyfills.ts")) {
+				entries["main.js"].unshift("./src/lib/polyfills.ts");
+			}
+			return entries;
+		};
+		return config;
+	},
 } satisfies NextConfig;
---
+++ src/lib/polyfills.ts
@@ -0,0 +1,3 @@
+"use client";
+
+if (!Object.groupBy) require("core-js/actual/object/group-by");
